d46c9afba791a627cd94d7ed6fabdbfba7e60e22
nmkolp <45236473+nmkolp@users.noreply.github.com>
2026-03-27 19:02:22 +0200

    Refactor timer.cmd for more consistent naming style

--- timer.cmd
+++ timer.cmd
@@ -1,6 +1,6 @@
-@echo off
+@ECHO OFF
 
-:: loops echo the bell character after specified minutes
+:: after specified minutes, loops echo the bell character
 
 :: the bell character is invisible
 SET bell=
@@ -13,28 +13,30 @@ IF [%1] == [] (
 
 FOR /L %%i IN (%input%,-1,1) DO (
   IF %%i==1 (
-    @ECHO Alarm starts after 1 minute
+    ECHO Alarm starts after 1 minute
   ) ELSE (
-    @ECHO Alarm starts after %%i minutes
+    ECHO Alarm starts after %%i minutes
   )
   TIMEOUT /T 60 /nobreak >nul
 )
 
-@ECHO Alarm started
+ECHO Alarm started
 
 CALL :loopBell
+
+GOTO :EOF
 
 :: Subroutines
 
 :loopBell
-  @ECHO %bell%
+  ECHO %bell%
   TIMEOUT /T 1 /nobreak >nul
   GOTO :loopBell
 
 :inputMinutes
   SET /P input="Enter minutes: "
-  @ECHO %input%| findstr "^[0-9][0-9]*$" >nul
-  IF errorlevel 1 (
+  ECHO %input%| findstr "^[0-9][0-9]*$" >nul
+  IF ERRORLEVEL 1 (
     GOTO :inputMinutes
   )
   EXIT /B
