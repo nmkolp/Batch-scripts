bf82046ba6275648cd768833c34b871d64ffd56e
nmkolp <45236473+nmkolp@users.noreply.github.com>
2026-03-27 18:33:18 +0200

    Fixed an issue with IF block variable expansion
    Batch is so cursed

--- timer.cmd
+++ timer.cmd
@@ -1,17 +1,12 @@
 @echo off
 
-:: after specified minutes loops echo the bell character
+:: loops echo the bell character after specified minutes
 
 :: the bell character is invisible
 SET bell=
 
 IF [%1] == [] (
-  :inputMinutes
-    SET /P input="Enter minutes: "
-    @ECHO %input%| findstr "^[0-9][0-9]*$" >nul
-    IF errorlevel 1 (
-      GOTO :inputMinutes
-    )
+  CALL :inputMinutes
 ) ELSE (
   SET input=%1
 )
@@ -27,7 +22,19 @@ FOR /L %%i IN (%input%,-1,1) DO (
 
 @ECHO Alarm started
 
+CALL :loopBell
+
+:: Subroutines
+
 :loopBell
   @ECHO %bell%
   TIMEOUT /T 1 /nobreak >nul
   GOTO :loopBell
+
+:inputMinutes
+  SET /P input="Enter minutes: "
+  @ECHO %input%| findstr "^[0-9][0-9]*$" >nul
+  IF errorlevel 1 (
+    GOTO :inputMinutes
+  )
+  EXIT /B
